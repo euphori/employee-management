f35a7c1171c4b54cd0c86514a0ded0c29e86b6d0
ItzJM28 <168981548+ItzJM28@users.noreply.github.com>
2024-05-19 21:42:33 +0800

    Updated edit_budget_screen

--- lib/features/user_auth/presentation/pages/edit_budget_screen.dart
+++ lib/features/user_auth/presentation/pages/edit_budget_screen.dart
@@ -13,25 +13,38 @@ class EditBudgetScreen extends StatelessWidget {
           },
         ),
       ),
-      backgroundColor: Colors.black, // Set the background color to black
-      body: SingleChildScrollView(
-        padding: EdgeInsets.symmetric(vertical: 50),
-        child: Center(
-          child: Container(
-            padding: EdgeInsets.all(16),
-            height: 148,
-            width: 307,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.5),
-                  spreadRadius: -1,
-                  blurRadius: 7.5,
-                  offset: Offset(0,3),
-                ),
-              ],
+      backgroundColor: Colors.transparent, // Set the background color to transparent
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.black, // Black color at the top
+              Colors.white, // White color at the bottom
+            ],
+            stops: [0.5, 0.5], // To make the transition abrupt
+          ),
+        ),
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(vertical: 50),
+          child: Center(
+            child: Container(
+              padding: EdgeInsets.all(16),
+              height: 148,
+              width: 357,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.5),
+                    spreadRadius: -1,
+                    blurRadius: 7.5,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
