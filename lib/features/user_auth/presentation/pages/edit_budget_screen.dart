b1a1ca0af7c2e5c2af33f0779889b3128fff5a29
ItzJM28 <168981548+ItzJM28@users.noreply.github.com>
2024-05-19 22:48:09 +0800

    Update edit_budget_screen.dart

--- lib/features/user_auth/presentation/pages/edit_budget_screen.dart
+++ lib/features/user_auth/presentation/pages/edit_budget_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class EditBudgetScreen extends StatelessWidget {
+  final double whiteHeightRatio;
+
+  EditBudgetScreen({this.whiteHeightRatio = 0.75});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,7 +17,7 @@ class EditBudgetScreen extends StatelessWidget {
           },
         ),
       ),
-      backgroundColor: Colors.transparent, // Set the background color to transparent
+      backgroundColor: Colors.transparent, // Make scaffold background transparent to show the gradient
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
@@ -23,28 +27,73 @@ class EditBudgetScreen extends StatelessWidget {
               Colors.black, // Black color at the top
               Colors.white, // White color at the bottom
             ],
-            stops: [0.5, 0.5], // To make the transition abrupt
+            stops: [1 - whiteHeightRatio, 1 - whiteHeightRatio], // Adjust transition point
           ),
         ),
         child: SingleChildScrollView(
           padding: EdgeInsets.symmetric(vertical: 50),
           child: Center(
-            child: Container(
-              padding: EdgeInsets.all(16),
-              height: 148,
-              width: 357,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.5),
-                    spreadRadius: -1,
-                    blurRadius: 7.5,
-                    offset: Offset(0, 3),
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(16), // Add padding outside the container
+                  child: Container(
+                    padding: EdgeInsets.all(16), // Internal padding of the container
+                    height: 148,
+                    width: 357,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.5),
+                          spreadRadius: -1,
+                          blurRadius: 7.5,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
                   ),
-                ],
-              ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only( top: 50), // Add padding outside the container
+                  child: Column(
+                    children: [
+                      Text('Today', style: TextStyle(fontWeight: FontWeight.bold),),
+                      Container(
+                        padding: EdgeInsets.all(16), // Internal padding of the container
+                        height: 148,
+                        width: 298,
+                        decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 30), // Add padding outside the container
+                  child: Column(
+                    children: [
+                      Text(
+                        'Yesterday',
+                         style: TextStyle(fontWeight: FontWeight.bold), ),
+                      
+                      Container(
+                        padding: EdgeInsets.all(16), // Internal padding of the container
+                        height: 148,
+                        width: 298,
+                        decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(10),
+                        ),
+                      )
+                    ],
+                    
+                  ),
+                ),
+              ],
             ),
           ),
         ),
